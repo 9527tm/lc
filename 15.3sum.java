d8766e45a00d347189282467172034760c5b0021
9527tm <52052033+9527tm@users.noreply.github.com>
2019-06-20 15:15:12 -0700

    update 15.3sum.java

--- 15.3sum.java
+++ 15.3sum.java
@@ -34,6 +34,11 @@
  */
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        //return sol1(nums);
+        return sol2(nums);
+    }
+    
+    private List<List<Integer>> sol1(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
@@ -56,6 +61,36 @@ class Solution {
                     else {
                         res.add(Arrays.asList(nums[i], nums[left], nums[right]));
                         left++;
+                        right--;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+    
+    private List<List<Integer>> sol2(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0) {
+                    left++;
+                }
+                else if (sum > 0) {
+                    right--;
+                }
+                else {
+                    res.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right + 1]) {
                         right--;
                     }
                 }
