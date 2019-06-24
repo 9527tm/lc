ea9e15ca6edf2142a206519f1d9742fe301fe847
9527tm <52052033+9527tm@users.noreply.github.com>
2019-06-23 17:34:51 -0700

    update 21.merge-two-sorted-lists.java

--- 21.merge-two-sorted-lists.java
+++ 21.merge-two-sorted-lists.java
@@ -31,6 +31,12 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        //return sol1(l1, l2);
+        return sol2(l1, l2);
+        //return sol22(l1, l2);
+    }
+
+    private ListNode sol1(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0);
         ListNode tail = dummyHead, p1 = l1, p2 = l2;
         while (p1 != null && p2 != null) {
@@ -46,5 +52,34 @@ class Solution {
         }
         tail.next = p1 != null ? p1 : p2;
         return dummyHead.next;
+    }
+
+    private ListNode sol2(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+        if (l1.val < l2.val) {
+            l1.next = sol2(l1.next, l2);
+            return l1;
+        }
+        l2.next = sol2(l1, l2.next);
+        return l2;
+    }
+
+    private ListNode sol22(ListNode l1, ListNode l2) {
+        if (l1 != null && l2 != null) {
+            ListNode next = sol2(l1.next, l2.next);
+            if (l2.val < l1.val) {
+                ListNode tmp = l1;
+                l1 = l2;
+                l2 = tmp;
+            }
+            l1.next = l2;
+            l2.next = next;
+        }
+        return l1 != null ? l1 : l2;
     }
 }
