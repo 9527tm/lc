841b5791113e1694494dbf41340688ada3aa134a
9527tm <52052033+9527tm@users.noreply.github.com>
2019-07-03 00:59:46 -0700

    update 75.sort-colors.java

--- 75.sort-colors.java
+++ 75.sort-colors.java
@@ -40,28 +40,29 @@
  */
 class Solution {
     public void sortColors(int[] nums) {
-        //sol0(nums, 3);
-        //sol1(nums);
-        //sol1a(nums);
-        //sol2(nums);
-        //sol2a(nums);
-        //sol2b(nums);
-        //sol3(nums);
-        //sol4(nums);
-        //sol5(nums);
-        //sol5a(nums);
-        //sol5b(nums);
-        //sol6(nums, 3);
-        //sol6a(nums, 3);
-        //sol7(nums, 3);
-        //sol8(nums, 3);
-        //sol9(nums, 3);
-        //sol10(nums, 3);
-        sol11(nums);
-        //sol11a(nums);
+        //SOLUTION         //VALUE //DETAIL
+        //sol0(nums, 3);   //5     //Count Sort
+        //sol1(nums);      //5     //DNF Dijkstra
+        //sol1a(nums);         //3 //sol1 -> sol1a: no swap
+        //sol2(nums);      //5     //CLRS 2 + 1 => 3 way, p171
+        //sol2a(nums);     //5     //sol2 -> sol2a: no swap
+        //sol2b(nums);     //5     //sol2a -> sol2b: if nested
+        //sol3(nums);          //3 //EPIJ, p43: 2 passes and 2 ways
+        //sol4(nums);          //1 //Created from sol3, but low efficiency
+        //sol5(nums);          //4 //LC top 1, left 2 + right 2 => 3 ways
+        //sol5a(nums);         //4 //sol5 -> sol5a: simplify loops
+        //sol5b(nums);         //3 //sol5a -> sol5b: immediately mid++, sol5b <=> sol1 TOTALLY
+        //sol5c(nums);         //0 //sol5b -> sol5c: Not good just delayed i++ / k--
+        //sol5d(nums);         //0 //sol5c -> sol5d: no swap
+        //sol6(nums, 3);   //5     //Recursion, 2 -> n ways
+        //sol6a(nums, 3);      //4 //sol6 -> sol6a: reversely
+        sol7(nums, 3);     //5     //Iterative, 1 pass, 3 -> n ways (sol2b)
+        //sol8(nums, 3);       //4 //sol7 -> sol8: sol2b -> sol2
+        //sol9(nums, 3);   //5     //Iterative, O(1) space, 3 -> n ways (sol1)
+        //sol10(nums, 3);      //4 //sol9 -> sol10: 2 -> n ways
     }
 
-    private void sol0(int[] nums, int k) {
+    private void sol0(int[] nums, int k) {//Count Sort
         int[] counts = new int[k];
         for (int n : nums) {
             counts[n]++;
@@ -74,7 +75,7 @@ class Solution {
         }
     }
 
-    private void sol1(int[] nums) {
+    private void sol1(int[] nums) {//DNF Dijkstra
         int i = 0, j = 0, k = nums.length - 1;
         while (j <= k) {
             if (nums[j] == 0) {
@@ -89,7 +90,7 @@ class Solution {
         }
     }
 
-    private void sol1a(int[] nums) {
+    private void sol1a(int[] nums) {//sol1 -> sol1a: no swap
         int i = 0, j = 0, k = nums.length - 1;
         while (j <= k) {
             if (nums[j] == 0) {
@@ -106,7 +107,7 @@ class Solution {
         }
     }
 
-    private void sol2(int[] nums) {
+    private void sol2(int[] nums) {//CLRS 2 + 1 => 3 way, p171
         int i = 0, j = 0, k = 0;
         for (int n : nums) {
             if (n == 0) {
@@ -122,7 +123,7 @@ class Solution {
         }
     }
 
-    private void sol2a(int[] nums) {
+    private void sol2a(int[] nums) {//sol2 -> sol2a: no swap
         int i = 0, j = 0, k = 0;
         for (int n : nums) {
             if (n == 0) {
@@ -140,7 +141,7 @@ class Solution {
         }
     }
 
-    private void sol2b(int[] nums) {
+    private void sol2b(int[] nums) {//sol2a -> sol2b (if nested)
         int i = 0, j = 0, k = 0;
         for (int n : nums) {
             if (n <= 2) {
@@ -155,11 +156,11 @@ class Solution {
         }
     }
 
-    private void sol3(int[] nums) {
+    private void sol3(int[] nums) {//EPIJ, p43
         int count0 = sol3(nums, 0, nums.length - 1, 0);
         int count1 = sol3(nums, count0, nums.length - 1, 1);
     }
-    private int sol3(int[] nums, int left, int right, int smaller) {
+    private int sol3(int[] nums, int left, int right, int smaller) {//Hoare, CLRS p185
         while (left <= right) {
             if (nums[left] == smaller) {
                 left++;
@@ -174,7 +175,7 @@ class Solution {
         return left;
     }
 
-    private void sol4(int[] nums) {
+    private void sol4(int[] nums) {//Created from sol3, but low efficiency
         int mid = sol4(nums, 0, nums.length - 1, 0, 2);
         sol3(nums, 0, mid - 1, 0);
         sol3(nums, mid, nums.length - 1, 1);
@@ -193,7 +194,7 @@ class Solution {
         }
         return left;
     }
-
+                                   //LC top 1, left 2 + right 2 -> 3 ways
     private void sol5(int[] nums) {///discuss/26472/Share-my-at-most-two-pass-constant-space-10-line-solution
         int left = 0, right = nums.length - 1; //[left, right]: from first 1 to last 1, 
         for (int i = left; i <= right; i++) {  //               both are inclusive.
@@ -206,7 +207,7 @@ class Solution {
         }
     }
 
-    private void sol5a(int[] nums) {
+    private void sol5a(int[] nums) {//sol5 -> sol5a: simplify loops
         int left = 0, mid = 0, right = nums.length - 1;
         while (mid <= right) {
             if (nums[mid] == 2) {
@@ -221,7 +222,7 @@ class Solution {
         }
     }
 
-    private void sol5b(int[] nums) { // sol5b() totally equal to <=> sol1()    
+    private void sol5b(int[] nums) { //sol5a -> sol5b:  immediately mid++, totally equal to <=> sol1()    
         int left = 0, mid = 0, right = nums.length - 1;
         while (mid <= right) {
             if (nums[mid] == 2) {
@@ -235,8 +236,44 @@ class Solution {
             }
         }
     }
+                                    //sol5b -> sol5c: delayed i++ / k--  when leading 0 or tailing 2
+                                    //but it is hard to read and has no gains since i and k are the same physical meanings as sol1.
+                                    //when algorithm completes, i and k are points to the first and last 1 if any. 
+                                    //So it's not really a good algorithm although many people on LC may think it in niubility.
+    private void sol5c(int[] nums) {///discuss/26637/Simple-one-pass-solution
+        int i = 0, j = 0, k = nums.length - 1; //test case: [0, 1, 0]
+        while (j <= k) {
+            if (nums[j] == 0 && j > i) {
+                swap(nums, i++, j);
+            }
+            else if (nums[j] == 2 && j < k) {
+                swap(nums, j, k--);
+            }
+            else {
+                j++;
+            }
+        }
+    }
+                                    //sol5c -> sol5d: no swap
+    private void sol5d(int[] nums) {///discuss/195342/O(n)-one-pass-Java-solution-beat-100
+        int i = 0, j = 0, k = nums.length - 1;
+        while (j <= k) {
+            if (nums[j] == 0 && j > i) {
+                nums[j] = nums[i];
+                nums[i++] = 0;
+            }
+            else if (nums[j] == 2 && j < k) {
+                nums[j] = nums[k];
+                nums[k--] = 2;
+            }
+            else {
+                j++;
+            }
+        }
+    }
 
-    private void sol6(int[] nums, int k) {//k >= 2, nums[i] belongs to [0, 1, 2, k - 1]
+    //k >= 2, nums[i] belongs to [0, 1, 2, k - 1]
+    private void sol6(int[] nums, int k) {//Recursion, 2 -> n ways
         sol6(nums, 0, nums.length - 1, 0, k);
     }
     private void  sol6(int[] nums, int left, int right, int target, int k) {
@@ -252,7 +289,7 @@ class Solution {
         sol6(nums, i, right, target + 1, k);
     }
 
-    private void sol6a(int[] nums, int k) {
+    private void sol6a(int[] nums, int k) {//sol6 -> sol6a: reversely
         sol6a(nums, 0, nums.length - 1, k - 1);
     }
     private void sol6a(int[] nums, int left, int right, int target) {
@@ -268,7 +305,7 @@ class Solution {
         sol6a(nums, left, i, target - 1);
     }
 
-    private void sol7(int[] nums, int k) {
+    private void sol7(int[] nums, int k) {//Iterative, 1 pass, 3 -> n ways (sol2b)
         int[] pointers = new int[k];
         for (int n : nums) {
             for (int i = k - 1; i >= n; i--) { // i >= 0 && i >= n
@@ -277,7 +314,7 @@ class Solution {
         }
     }
 
-    private void sol8(int[] nums, int k) {
+    private void sol8(int[] nums, int k) {//sol7 -> sol8: sol2b -> sol2
         int[] pointers = new int[k];
         for (int n : nums) {
             int j = k - 1;
@@ -289,7 +326,7 @@ class Solution {
             pointers[j]++;
         }
     }
-
+                                          //Iterative, O(1) space, 3 -> n ways (sol1)
     private void sol9(int[] nums, int k) {//discuss/26654/Sort-colors-and-sort-k-colors-C++-solution
         int left = 0, right = nums.length - 1;//www.lintcode.com/en/problem/sort-colors-ii/
         for (int minColor = 0, maxColor = k - 1; minColor < maxColor; minColor++, maxColor--) {
@@ -310,7 +347,7 @@ class Solution {
         }
     }
         
-    private void sol10(int[] nums, int k) {
+    private void sol10(int[] nums, int k) {//sol9 -> sol10: 2 -> n ways
         int left = 0, right = nums.length - 1;
         for (int minColor = 0; minColor < k - 1; minColor++) {
             int i = left;
@@ -320,38 +357,6 @@ class Solution {
                 }
             }
             left = i;
-        }
-    }
-
-    private void sol11(int[] nums) {///discuss/26637/Simple-one-pass-solution
-        int i = 0, j = 0, k = nums.length - 1; //test case: [0, 1, 0]
-        while (j <= k) {
-            if (nums[j] == 0 && j > i) {
-                swap(nums, i++, j);
-            }
-            else if (nums[j] == 2 && j < k) {
-                swap(nums, j, k--);
-            }
-            else {
-                j++;
-            }
-        }
-    }
-
-    private void sol11a(int[] nums) {///discuss/195342/O(n)-one-pass-Java-solution-beat-100
-        int i = 0, j = 0, k = nums.length - 1;
-        while (j <= k) {
-            if (nums[j] == 0 && j > i) {
-                nums[j] = nums[i];
-                nums[i++] = 0;
-            }
-            else if (nums[j] == 2 && j < k) {
-                nums[j] = nums[k];
-                nums[k--] = 2;
-            }
-            else {
-                j++;
-            }
         }
     }
 
