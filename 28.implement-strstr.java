b83692fd8a5fad88f49c869786e4e97c27238798
9527tm <52052033+9527tm@users.noreply.github.com>
2019-07-06 15:15:53 -0700

    update 28.implement-strstr.java

--- 28.implement-strstr.java
+++ 28.implement-strstr.java
@@ -62,15 +62,11 @@ class Solution {
         return true;
     }
 
-    //final long smallPrime = 11L, bigPrime = 257L;
-    final long smallPrime = 37L, bigPrime = 104729L;
+    final long smallPrime = 11L, bigPrime = 257L;
 
     private int sol2(String haystack, String needle) {
         if (needle.length() <= 0) {
             return 0;
-        }
-        if (needle.length() > haystack.length()) {
-            return -1;
         }
         long needleHashCode = 0L, baseFactor = 1L;
         for (int i = 0; i < needle.length(); i++) {
