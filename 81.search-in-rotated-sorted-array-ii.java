10a7b64361d155cd21ef9eef29caaf9ebb1359f3
9527tm <52052033+9527tm@users.noreply.github.com>
2019-07-16 19:11:19 -0700

    update 81.search-in-rotated-sorted-array-ii.java

---
+++ 81.search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode id=81 lang=java
+ *
+ * [81] Search in Rotated Sorted Array II
+ *
+ * https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
+ *
+ * algorithms
+ * Medium (32.65%)
+ * Total Accepted:    178.5K
+ * Total Submissions: 546K
+ * Testcase Example:  '[2,5,6,0,0,1,2]\n0'
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown
+ * to you beforehand.
+ * 
+ * (i.e., [0,0,1,2,2,5,6] might become [2,5,6,0,0,1,2]).
+ * 
+ * You are given a target value to search. If found in the array return true,
+ * otherwise return false.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [2,5,6,0,0,1,2], target = 0
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [2,5,6,0,0,1,2], target = 3
+ * Output: false
+ * 
+ * Follow up:
+ * 
+ * 
+ * This is a follow up problem to Search in Rotated Sorted Array, where nums
+ * may contain duplicates.
+ * Would this affect the run-time complexity? How and why?
+ * 
+ * 
+ */
+class Solution {
+    public boolean search(int[] nums, int target) {
+        return sol1(nums, target);    
+    }
+    private boolean sol1(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] == nums[right]) {
+                left++;
+                continue;
+            }
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+            else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return left > right ? false : nums[left] == target;
+    }
+}
