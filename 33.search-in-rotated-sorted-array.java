2a60324e68eadf65245c328cffce758c22f25400
9527tm <52052033+9527tm@users.noreply.github.com>
2019-07-22 12:02:51 -0700

    update 33.search-in-rotated-sorted-array.java

--- 33.search-in-rotated-sorted-array.java
+++ 33.search-in-rotated-sorted-array.java
@@ -67,4 +67,52 @@ class Solution {
         }
         return -1;
     }
+
+    private int sol2(int[] nums, int target) {
+        int start = findIndexOfMinimum(nums);
+        int res1 = binarySearch(nums, 0, start - 1, 0, target);
+        int res2 = binarySearch(nums, start, nums.length - 1, 0, target);
+        return res1 >= 0 ? res1 : res2;
+    }
+
+    private int sol3(int[] nums, int target) {
+        int start = findIndexOfMinimum(nums);
+        return binarySearch(nums, 0, nums.length - 1, start, target);
+    }
+
+    private int findIndexOfMinimum(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            }
+            else if (nums[mid] < nums[left]) {
+                right = mid;
+            }
+            else {//nums[left] < nums[mid] < nums[right]
+                break;
+            }
+        }
+        return left; //when nums.length = 0, we return left = 0.
+    }
+
+    private int binarySearch(int[] nums, int left, int right, int rotateStep, int target) {
+        left += rotateStep;
+        right += rotateStep;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int midValue = nums[mid % nums.length];
+            if (midValue == target) {
+                return mid % nums.length;
+            }
+            else if (midValue < target) {
+                left = mid + 1;
+            }
+            else {//(midValue > target)
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
 }
