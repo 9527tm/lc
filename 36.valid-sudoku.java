b9f6fec011bb29b5368746d2ac65a782c957f458
9527tm <52052033+9527tm@users.noreply.github.com>
2019-07-28 22:46:58 -0700

    update 36.valid-sudoku.java

---
+++ 36.valid-sudoku.java
@@ -0,0 +1,122 @@
+/*
+ * @lc app=leetcode id=36 lang=java
+ *
+ * [36] Valid Sudoku
+ *
+ * https://leetcode.com/problems/valid-sudoku/description/
+ *
+ * algorithms
+ * Medium (43.36%)
+ * Total Accepted:    251.7K
+ * Total Submissions: 573.4K
+ * Testcase Example:  '[["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]'
+ *
+ * Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be
+ * validated according to the following rules:
+ * 
+ * 
+ * Each row must contain the digits 1-9 without repetition.
+ * Each column must contain the digits 1-9 without repetition.
+ * Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without
+ * repetition.
+ * 
+ * 
+ * 
+ * A partially filled sudoku which is valid.
+ * 
+ * The Sudoku board could be partially filled, where empty cells are filled
+ * with the character '.'.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * [
+ * ⁠ ["5","3",".",".","7",".",".",".","."],
+ * ⁠ ["6",".",".","1","9","5",".",".","."],
+ * ⁠ [".","9","8",".",".",".",".","6","."],
+ * ⁠ ["8",".",".",".","6",".",".",".","3"],
+ * ⁠ ["4",".",".","8",".","3",".",".","1"],
+ * ⁠ ["7",".",".",".","2",".",".",".","6"],
+ * ⁠ [".","6",".",".",".",".","2","8","."],
+ * ⁠ [".",".",".","4","1","9",".",".","5"],
+ * ⁠ [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * Output: true
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * [
+ * ["8","3",".",".","7",".",".",".","."],
+ * ["6",".",".","1","9","5",".",".","."],
+ * [".","9","8",".",".",".",".","6","."],
+ * ["8",".",".",".","6",".",".",".","3"],
+ * ["4",".",".","8",".","3",".",".","1"],
+ * ["7",".",".",".","2",".",".",".","6"],
+ * [".","6",".",".",".",".","2","8","."],
+ * [".",".",".","4","1","9",".",".","5"],
+ * [".",".",".",".","8",".",".","7","9"]
+ * ]
+ * Output: false
+ * Explanation: Same as Example 1, except with the 5 in the top left corner
+ * being 
+ * ⁠   modified to 8. Since there are two 8's in the top left 3x3 sub-box, it
+ * is invalid.
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * A Sudoku board (partially filled) could be valid but is not necessarily
+ * solvable.
+ * Only the filled cells need to be validated according to the mentioned
+ * rules.
+ * The given board contain only digits 1-9 and the character '.'.
+ * The given board size is always 9x9.
+ * 
+ * 
+ */
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        return sol1(board);       
+    }
+    private boolean sol1(char[][] board) {
+        Set<Character> set = new HashSet<>();
+        int n = board.length, m = n / 3; //n = 9, m = 3
+        //1. check rows
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] != '.' && !set.add(board[i][j])) {
+                    return false;
+                }
+            }
+            set.clear();
+        }
+        //2. check cols
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < n; i++) {
+                if (board[i][j] != '.' && !set.add(board[i][j])) {
+                    return false;
+                }
+            }
+            set.clear();
+        }
+        //3. check sub-boxes
+        for (int i = 0; i < n; i += m) {
+            for (int j = 0; j < n; j += m) {
+                for (int k = i; k < i + m; k++) {
+                    for (int l = j; l < j + m; l++) {
+                        if (board[k][l] != '.' && !set.add(board[k][l])) {
+                            return false;
+                        }
+                    }
+                }
+                set.clear();
+            }
+        }
+        return true;
+    }
+}
