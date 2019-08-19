6671fa9bb58b5b05d2c2396cba404528f5c56ff4
9527tm <52052033+9527tm@users.noreply.github.com>
2019-08-18 23:32:22 -0700

    update 572.subtree-of-another-tree.java

---
+++ 572.subtree-of-another-tree.java
@@ -0,0 +1,90 @@
+/*
+ * @lc app=leetcode id=572 lang=java
+ *
+ * [572] Subtree of Another Tree
+ *
+ * https://leetcode.com/problems/subtree-of-another-tree/description/
+ *
+ * algorithms
+ * Easy (41.79%)
+ * Total Accepted:    110.9K
+ * Total Submissions: 263.8K
+ * Testcase Example:  '[3,4,5,1,2]\n[4,1,2]'
+ *
+ * 
+ * Given two non-empty binary trees s and t, check whether tree t has exactly
+ * the same structure and node values with a subtree of s. A subtree of s is a
+ * tree consists of a node in s and all of this node's descendants. The tree s
+ * could also be considered as a subtree of itself.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Given tree s:
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \
+ * ⁠1   2
+ * 
+ * Given tree t:
+ * 
+ * ⁠  4 
+ * ⁠ / \
+ * ⁠1   2
+ * 
+ * Return true, because t has the same structure and node values with a subtree
+ * of s.
+ * 
+ * 
+ * Example 2:
+ * 
+ * Given tree s:
+ * 
+ * ⁠    3
+ * ⁠   / \
+ * ⁠  4   5
+ * ⁠ / \
+ * ⁠1   2
+ * ⁠   /
+ * ⁠  0
+ * 
+ * Given tree t:
+ * 
+ * ⁠  4
+ * ⁠ / \
+ * ⁠1   2
+ * 
+ * Return false.
+ * 
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        return sol1(s, t); 
+    }
+    private boolean sol1(TreeNode s, TreeNode t) {
+        if (s == null) {
+            return false;
+        }
+        return sol1Match(s, t) || sol1(s.left, t) || sol1(s.right, t);
+    }
+    private boolean sol1Match(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        else if (s == null || t == null) {
+            return false;
+        }
+        return (s.val == t.val) && sol1Match(s.left, t.left) && sol1Match(s.right, t.right);
+    }
+}
