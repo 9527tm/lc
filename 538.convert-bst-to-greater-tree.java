623f6e50c8f86b3886c12d80947179db164bf18f
9527tm <52052033+9527tm@users.noreply.github.com>
2019-08-18 23:03:11 -0700

    update 538.convert-bst-to-greater-tree.java

--- 538.convert-bst-to-greater-tree.java
+++ 538.convert-bst-to-greater-tree.java
@@ -41,8 +41,10 @@
  */
 class Solution {
     public TreeNode convertBST(TreeNode root) {
-        return sol1(root); 
+        //return sol1(root); 
+        return sol2(root);
     }
+
     private TreeNode sol1(TreeNode root) {
         sol1(root, new int[1]);
         return root;
@@ -56,4 +58,20 @@ class Solution {
         root.val = sum[0];
         sol1(root.left, sum);
     }
+
+    private TreeNode sol2(TreeNode root) {
+        return sol2(root, new int[1]);
+    }
+    private TreeNode sol2(TreeNode root, int[] sum) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode root2 = new TreeNode(0); //default value intialization
+        root2.right = sol2(root.right, sum);
+        sum[0] += root.val;
+        root2.val = sum[0];
+        root2.left = sol2(root.left, sum);
+        return root2;
+    }
+
 }
