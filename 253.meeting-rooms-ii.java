2aac782f0874afe7df8c092e5ff581d26a2e7e10
9527tm <52052033+9527tm@users.noreply.github.com>
2019-10-02 20:11:02 -0700

    update 253.meeting-rooms-ii.java

---
+++ 253.meeting-rooms-ii.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=253 lang=java
+ *
+ * [253] Meeting Rooms II
+ *
+ * https://leetcode.com/problems/meeting-rooms-ii/description/
+ *
+ * algorithms
+ * Medium (43.68%)
+ * Total Accepted:    189K
+ * Total Submissions: 432.6K
+ * Testcase Example:  '[[0,30],[5,10],[15,20]]'
+ *
+ * Given an array of meeting time intervals consisting of start and end times
+ * [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms
+ * required.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [[0, 30],[5, 10],[15, 20]]
+ * Output: 2
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [[7,10],[2,4]]
+ * Output: 1
+ * 
+ * NOTE: input types have been changed on April 15, 2019. Please reset to
+ * default code definition to get new method signature.
+ * 
+ */
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        return sol1(intervals);        
+        /*
+        return sol2(intervals);        
+        return sol3(intervals);        
+        return sol4(intervals);        
+        return sol5(intervals);
+        */       
+    }
+
+    private int sol1(int[][] intervals) {
+        Arrays.sort(intervals, (itv1, itv2) -> Integer.compare(itv1[0], itv2[0]));
+        PriorityQueue<int[]> minHeap = new PriorityQueue<>((itv1, itv2) -> Integer.compare(itv1[1], itv2[1]));
+        for (int[] itv : intervals) {
+            if (!minHeap.isEmpty() && minHeap.peek()[1] <= itv[0]) {
+                minHeap.poll();
+            }
+            minHeap.offer(itv);
+        }
+        return minHeap.size();
+    }
+}
