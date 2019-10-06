a78647ca51810bf7dcc914c0b1e9eb5df50ea087
9527tm <52052033+9527tm@users.noreply.github.com>
2019-10-06 14:42:58 -0700

    update 48.rotate-image.java

---
+++ 48.rotate-image.java
@@ -0,0 +1,87 @@
+/*
+ * @lc app=leetcode id=48 lang=java
+ *
+ * [48] Rotate Image
+ *
+ * https://leetcode.com/problems/rotate-image/description/
+ *
+ * algorithms
+ * Medium (50.81%)
+ * Total Accepted:    295.1K
+ * Total Submissions: 579.7K
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * You are given an n x n 2D matrix representing an image.
+ * 
+ * Rotate the image by 90 degrees (clockwise).
+ * 
+ * Note:
+ * 
+ * You have to rotate the image in-place, which means you have to modify the
+ * input 2D matrix directly. DO NOT allocate another 2D matrix and do the
+ * rotation.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Given input matrix = 
+ * [
+ * ⁠ [1,2,3],
+ * ⁠ [4,5,6],
+ * ⁠ [7,8,9]
+ * ],
+ * 
+ * rotate the input matrix in-place such that it becomes:
+ * [
+ * ⁠ [7,4,1],
+ * ⁠ [8,5,2],
+ * ⁠ [9,6,3]
+ * ]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Given input matrix =
+ * [
+ * ⁠ [ 5, 1, 9,11],
+ * ⁠ [ 2, 4, 8,10],
+ * ⁠ [13, 3, 6, 7],
+ * ⁠ [15,14,12,16]
+ * ], 
+ * 
+ * rotate the input matrix in-place such that it becomes:
+ * [
+ * ⁠ [15,13, 2, 5],
+ * ⁠ [14, 3, 4, 1],
+ * ⁠ [12, 6, 8, 9],
+ * ⁠ [16, 7,10,11]
+ * ]
+ * 
+ * 
+ */
+class Solution {
+    public void rotate(int[][] matrix) {
+        sol1(matrix);        
+    }
+
+    private void sol1(int[][] matrix) {
+        if (matrix.length <= 0) {
+            return;
+        }
+        int left = 0, right = matrix.length - 1, top = left, bottom = right;
+        while (left < right) {
+            for (int i = 0; i < right - left; i++) {
+                int temp = matrix[top][left + i];
+                matrix[top][left + i] = matrix[bottom - i][left];
+                matrix[bottom - i][left] = matrix[bottom][right - i];
+                matrix[bottom][right - i] = matrix[top + i][right];
+                matrix[top + i][right] = temp;
+            }
+            left++;
+            right--;
+            top++;
+            bottom--;
+        }
+    }
+}
