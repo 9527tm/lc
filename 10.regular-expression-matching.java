f62fc093d1c154ee4992791bb4b51f8ea7caad16
9527tm <52052033+9527tm@users.noreply.github.com>
2019-10-06 20:30:30 -0700

    update 10.regular-expression-matching.java

---
+++ 10.regular-expression-matching.java
@@ -0,0 +1,107 @@
+/*
+ * @lc app=leetcode id=10 lang=java
+ *
+ * [10] Regular Expression Matching
+ *
+ * https://leetcode.com/problems/regular-expression-matching/description/
+ *
+ * algorithms
+ * Hard (25.73%)
+ * Total Accepted:    347.1K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '"aa"\n"a"'
+ *
+ * Given an input string (s) and a pattern (p), implement regular expression
+ * matching with support for '.' and '*'.
+ * 
+ * 
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ * 
+ * 
+ * The matching should cover the entire input string (not partial).
+ * 
+ * Note:
+ * 
+ * 
+ * s could be empty and contains only lowercase letters a-z.
+ * p could be empty and contains only lowercase letters a-z, and characters
+ * like . or *.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ * Explanation: "a" does not match the entire string "aa".
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input:
+ * s = "aa"
+ * p = "a*"
+ * Output: true
+ * Explanation: '*' means zero or more of the preceding element, 'a'.
+ * Therefore, by repeating 'a' once, it becomes "aa".
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input:
+ * s = "ab"
+ * p = ".*"
+ * Output: true
+ * Explanation: ".*" means "zero or more (*) of any character (.)".
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input:
+ * s = "aab"
+ * p = "c*a*b"
+ * Output: true
+ * Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore,
+ * it matches "aab".
+ * 
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * Output: false
+ * 
+ * 
+ */
+class Solution {
+    public boolean isMatch(String s, String p) {
+        return sol1(s, p);        
+    }
+
+    private boolean sol1(String s, String p) {
+        return sol1(s, p, 0, 0);
+    }
+    private boolean sol1(String s, String p, int i, int j) {
+        if (i >= s.length() && j >= p.length()) {
+            return true;
+        }
+        if (j >= p.length()) {
+            return false;
+        }
+        //H.W.: forgot to handle '*' first. caveat: NEVER seperate '*' from its preceding char.
+        boolean flag = (i < s.length() && (s.charAt(i) == p.charAt(j) || '.' == p.charAt(j)));
+        if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+            return sol1(s, p, i, j + 2) || (flag && sol1(s, p, i + 1, j));
+        }
+
+        return flag && sol1(s, p, i + 1, j + 1);
+    }
+}
