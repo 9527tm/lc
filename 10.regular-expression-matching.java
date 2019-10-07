a1c12fffd7987b285a86ec6967e5a09d27fe0ea1
9527tm <52052033+9527tm@users.noreply.github.com>
2019-10-06 21:44:45 -0700

    update 10.regular-expression-matching.java

--- 10.regular-expression-matching.java
+++ 10.regular-expression-matching.java
@@ -83,7 +83,8 @@
  */
 class Solution {
     public boolean isMatch(String s, String p) {
-        return sol1(s, p);        
+        //return sol1(s, p);        
+        return sol2(s, p);        
     }
 
     private boolean sol1(String s, String p) {
@@ -103,5 +104,29 @@ class Solution {
         }
 
         return flag && sol1(s, p, i + 1, j + 1);
+    }
+
+    private boolean sol2(String s, String p) {
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+        for (int j = 1; j < dp[0].length; j++) {//H.W.: j = 0 (j should start from 1)  
+            dp[0][j] = p.charAt(j - 1) == '*' && dp[0][j - 2];
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2];
+                    if (s.charAt(i - 1) == p.charAt(j - 2) || '.' == p.charAt(j - 2)) {
+                        dp[i][j] |= dp[i - 1][j];
+                    }
+                }
+                else {
+                    if (s.charAt(i - 1) == p.charAt(j - 1) || '.' == p.charAt(j - 1)) {
+                        dp[i][j] = dp[i - 1][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
     }
 }
