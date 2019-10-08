cf8f03333aea8f0c263bb6c819906fcb7f12e400
9527tm <52052033+9527tm@users.noreply.github.com>
2019-10-08 09:39:32 -0700

    update 47.permutations-ii.java

--- 47.permutations-ii.java
+++ 47.permutations-ii.java
@@ -30,8 +30,9 @@
 class Solution {
     //https://www.cnblogs.com/grandyang/p/4359825.html
     public List<List<Integer>> permuteUnique(int[] nums) {
-        return sol1(nums); //How to dedup? Opt 1: scan a new number from tried number subarray [O(N!*N) / O(N)]
-        //return sol2(nums);   //              Opt 2: add a new number to tried number set [O(N!*N) / O(N*N)]
+        //return sol1(nums); //How to dedup? Opt 1: scan a new number from tried number subarray [O(N!*N) / O(N)]
+        //return sol2(nums); //              Opt 2: add a new number to tried number set [O(N!*N) / O(N*N)]
+        return sol3(nums);   //              Opt 3: sort and find next permutations [O(N!*N) / O(1)]
     }
 
     private List<List<Integer>> sol1(int[] nums) {
@@ -72,6 +73,7 @@ class Solution {
     }
 
     private List<List<Integer>> sol2(int[] nums) {
+        //No sort required: //https://www.cnblogs.com/grandyang/p/4359825.html#4314885
         List<List<Integer>> res = new ArrayList<>();
         sol2(nums, 0, new ArrayList<>(), res);
         return res;
@@ -93,5 +95,56 @@ class Solution {
             swap(nums, i, j);
             list.remove(list.size() - 1);
         }
+    }
+
+    private List<List<Integer>> sol3(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        do {
+            res.add(array2List(nums));
+        } while (nextPermutation(nums));
+        return res;
+    }
+
+    //step 1: two negative scans: one for lower, the other for greater
+    //step 2: exchange the lower with the greater => increase the smallest delta value
+    //step 3: reverse rear range (lower, end) => reset from largest to least number
+    private boolean nextPermutation(int[] nums) {//123 -> 132 -> 
+        //step 1.1: first pass scan for lower digit
+        int i = nums.length - 1;                 //[(1)32 -> (1)3(2) -> (2)3(1) -> 2(31) -> 2(13)]
+        while (i > 0 && nums[i] <= nums[i - 1]) {//scan the descending range from back to forth
+            i--;
+        }
+        if (i <= 0) {
+            return false;//no lower digit can be prompted, like: 321.
+        }//here the case: 132
+        int j = i - 1; //the lower digit found at j: (1)32
+
+        //step 1.2: second pass scan for greater digit
+        i = nums.length - 1;
+        while (i > j && nums[i] <= nums[j]) {
+            i--;
+        }//the greater digit found at i: 13(2)
+
+        //step 2: exchange lower with greater
+        swap(nums, i, j); //(1)3(2) -> (2)3(1) 
+
+        //step 3: reverse the rear range
+        reverse(nums, j + 1, nums.length - 1);//2(31) -> 2(13)
+        return true;
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            swap(nums, left++, right--);
+        }
+    }        
+
+    private List<Integer> array2List(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int n : nums) {
+            list.add(n);
+        }
+        return list;
     }
 }
