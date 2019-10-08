22dae78eced1fbb6fbc340c0acf9a836038345a5
9527tm <52052033+9527tm@users.noreply.github.com>
2019-10-08 10:07:14 -0700

    update 39.combination-sum.java

---
+++ 39.combination-sum.java
@@ -0,0 +1,80 @@
+/*
+ * @lc app=leetcode id=39 lang=java
+ *
+ * [39] Combination Sum
+ *
+ * https://leetcode.com/problems/combination-sum/description/
+ *
+ * algorithms
+ * Medium (50.96%)
+ * Total Accepted:    404.4K
+ * Total Submissions: 791.4K
+ * Testcase Example:  '[2,3,6,7]\n7'
+ *
+ * Given a set of candidate numbers (candidates) (without duplicates) and a
+ * target number (target), find all unique combinations in candidates where the
+ * candidate numbers sums to target.
+ * 
+ * The same repeated number may be chosen from candidates unlimited number of
+ * times.
+ * 
+ * Note:
+ * 
+ * 
+ * All numbers (including target) will be positive integers.
+ * The solution set must not contain duplicate combinations.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: candidates = [2,3,6,7], target = 7,
+ * A solution set is:
+ * [
+ * ⁠ [7],
+ * ⁠ [2,2,3]
+ * ]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: candidates = [2,3,5], target = 8,
+ * A solution set is:
+ * [
+ * [2,2,2,2],
+ * [2,3,3],
+ * [3,5]
+ * ]
+ * 
+ * 
+ */
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        return sol1(candidates, target);       
+    }
+
+    private List<List<Integer>> sol1(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        sol1(candidates, target, 0, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void sol1(int[] candidates, int target, int i, List<Integer> list, List<List<Integer>> res) {
+        if (i >= candidates.length || target <= 0) {
+            if (target == 0) {
+                res.add(new ArrayList<>(list));
+            }
+            return;
+        }
+        for (int j = 0; j * candidates[i] <= target; j++) {//H.W.: j > 0
+            if (j > 0) {
+                list.add(candidates[i]);
+            }
+            sol1(candidates, target - j * candidates[i], i + 1, list, res);
+        }
+        for (int j = 1; j * candidates[i] <= target; j++) {
+            list.remove(list.size() - 1);
+        }
+    }    
+}
