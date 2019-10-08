1c52a964e55df3bb9480dc8bb6553a10bb53bd5c
9527tm <52052033+9527tm@users.noreply.github.com>
2019-10-07 22:58:34 -0700

    update 46.permutations.java

---
+++ 46.permutations.java
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=46 lang=java
+ *
+ * [46] Permutations
+ *
+ * https://leetcode.com/problems/permutations/description/
+ *
+ * algorithms
+ * Medium (57.67%)
+ * Total Accepted:    446K
+ * Total Submissions: 771.6K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * Given a collection of distinct integers, return all possible permutations.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: [1,2,3]
+ * Output:
+ * [
+ * ⁠ [1,2,3],
+ * ⁠ [1,3,2],
+ * ⁠ [2,1,3],
+ * ⁠ [2,3,1],
+ * ⁠ [3,1,2],
+ * ⁠ [3,2,1]
+ * ]
+ * 
+ * 
+ */
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        return sol1(nums); 
+    }
+
+    private List<List<Integer>> sol1(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        sol1(nums, 0, res);
+        return res;
+    }
+
+    private void sol1(int[] nums, int i, List<List<Integer>> res) {
+        if (i >= nums.length) {
+            //H.W.: wrongly use "res.add(Arrays.asList(nums));" 
+            List<Integer> list = new ArrayList<>();
+            for (int j = 0; j < nums.length; j++) {
+                list.add(nums[j]);
+            }
+            res.add(list);
+        }
+        for (int j = i; j < nums.length; j++) {
+            swap(nums, i, j);
+            sol1(nums, i + 1, res);
+            swap(nums, i, j);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
