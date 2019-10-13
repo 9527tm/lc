c412120a0a0d79f9761e77302692c71bd8621ae2
9527tm <52052033+9527tm@users.noreply.github.com>
2019-10-12 18:46:39 -0700

    update 322.coin-change.java

---
+++ 322.coin-change.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode id=322 lang=java
+ *
+ * [322] Coin Change
+ *
+ * https://leetcode.com/problems/coin-change/description/
+ *
+ * algorithms
+ * Medium (31.96%)
+ * Total Accepted:    253.6K
+ * Total Submissions: 789.3K
+ * Testcase Example:  '[1,2,5]\n11'
+ *
+ * You are given coins of different denominations and a total amount of money
+ * amount. Write a function to compute the fewest number of coins that you need
+ * to make up that amount. If that amount of money cannot be made up by any
+ * combination of the coins, return -1.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: coins = [1, 2, 5], amount = 11
+ * Output: 3 
+ * Explanation: 11 = 5 + 5 + 1
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: coins = [2], amount = 3
+ * Output: -1
+ * 
+ * 
+ * Note:
+ * You may assume that you have an infinite number of each kind of coin.
+ * 
+ */
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        return sol1(coins, amount);
+    }
+
+    private int sol1(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int weight : coins) {
+                if (weight <= i && dp[i - weight] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[i - weight] + 1);
+                }
+            }
+        }
+        return dp[amount] != Integer.MAX_VALUE ? dp[amount] : -1;
+    }
+}
+
