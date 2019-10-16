959369c38b76775881daeda314a633e778e0a896
9527tm <9527tm@gmail.com>
2019-10-16 13:15:00 -0700

    update 127.word-ladder.java

--- 127.word-ladder.java
+++ 127.word-ladder.java
@@ -1,79 +1,13 @@
-/*
- * @lc app=leetcode id=127 lang=java
- *
- * [127] Word Ladder
- *
- * https://leetcode.com/problems/word-ladder/description/
- *
- * algorithms
- * Medium (25.73%)
- * Total Accepted:    309K
- * Total Submissions: 1.2M
- * Testcase Example:  '"hit"\n"cog"\n["hot","dot","dog","lot","log","cog"]'
- *
- * Given two words (beginWord and endWord), and a dictionary's word list, find
- * the length of shortest transformation sequence from beginWord to endWord,
- * such that:
- * 
- * 
- * Only one letter can be changed at a time.
- * Each transformed word must exist in the word list. Note that beginWord is
- * not a transformed word.
- * 
- * 
- * Note:
- * 
- * 
- * Return 0 if there is no such transformation sequence.
- * All words have the same length.
- * All words contain only lowercase alphabetic characters.
- * You may assume no duplicates in the word list.
- * You may assume beginWord and endWord are non-empty and are not the same.
- * 
- * 
- * Example 1:
- * 
- * 
- * Input:
- * beginWord = "hit",
- * endWord = "cog",
- * wordList = ["hot","dot","dog","lot","log","cog"]
- * 
- * Output: 5
- * 
- * Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" ->
- * "dog" -> "cog",
- * return its length 5.
- * 
- * 
- * Example 2:
- * 
- * 
- * Input:
- * beginWord = "hit"
- * endWord = "cog"
- * wordList = ["hot","dot","dog","lot","log"]
- * 
- * Output: 0
- * 
- * Explanation: The endWord "cog" is not in wordList, therefore no possible
- * transformation.
- * 
- * 
- * 
- * 
- * 
- */
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         //return sol1(beginWord, endWord, wordList);
         return sol2(beginWord, endWord, wordList);
     }
-
+    
     //BFS: O(n * m * c) / O(n)
     //     n -- size(wordList), m -- max(len(word)), c -- 26('a' - 'z')
     //or,
-    //     O((b ^ d) * m * c) / O(b ^ d)
+    //     O(b ^ d) / O(b ^ d)
     //     b -- branching factor (the average outdegree)
     //     d -- transforming distance from beginWord to endWord
     //     the worst case is O(b ^ d) = O(n) and d is the tree height h.
@@ -86,11 +20,11 @@ class Solution {
         Set<String> wordSet = new HashSet<>(wordList);
         Queue<String> queue = new LinkedList<>();
         queue.offer(beginWord);
-        for (int depth = 1; !queue.isEmpty(); depth++) {//depth is initialized:
+        for (int depth = 1; !queue.isEmpty(); depth++) {//depth is initialized: 
             int size = queue.size();                    //to 1 if check goal when expanding;
             for (int num = 0; num < size; num++) {      //to 2 if check goal when generating.
                 String currWord = queue.poll();
-                if (currWord.equals(endWord)) {//beginWord != endWord intially,
+                if (currWord.equals(endWord)) {//beginWord != endWord intially, 
                     return depth;              //so they must be transformed along wordList.
                 }                              //Don't worry about that endWord is not in wordList.
                 char[] array = currWord.toCharArray();
@@ -98,7 +32,7 @@ class Solution {
                     char currChar = array[i];
                     for (char ch = 'a'; ch <= 'z'; ch++) {
                         array[i] = ch;
-                        String nextWord = new String(array);
+                        String nextWord = new String(array); 
                         if (wordSet.remove(nextWord)) {//H.W.: used a visitedSet
                             queue.offer(nextWord);     //      but forgot to check wordSet first
                         }                              //TIP: remove wordSet only!
@@ -109,8 +43,8 @@ class Solution {
         }
         return 0;
     }
-
-    //BFS Bidirectional:     O(2 * (b ^ (d / 2)) * m * c) / O(2 * b ^ (d / 2))
+    
+    //BFS Bidirectional:     O(2 * (b ^ (d / 2))) / O(2 * b ^ (d / 2))
     //                       (2 * b ^ (d / 2)) is much smaller than (b ^ d)
     private int sol2(String beginWord, String endWord, List<String> wordList) {
         Set<String> wordSet = new HashSet<>(wordList);
@@ -121,7 +55,7 @@ class Solution {
         Set<String> dstSet = new HashSet<>();
         srcSet.add(beginWord);
         dstSet.add(endWord);
-
+       
         for (int depth = 2; !srcSet.isEmpty() && !dstSet.isEmpty(); depth++) {
             Set<String> tempSet = new HashSet<>();
             for (String currWord : srcSet) {
@@ -136,12 +70,12 @@ class Solution {
                         }                               //        the opposite search!
                         if (wordSet.remove(nextWord)) { //so, we need to check endWord in wordSet
                             tempSet.add(nextWord);      //    for a valid induction base:
-                        }                               //    if at least one of beginWord and endWord is
+                        }                               //    if at least one of beginWord and endWord is 
                     }                                   //    in wordSet, both are transformed.
                     array[i] = backupChar;              //    since beginWord may not be in wordSet,
                 }                                       //    endWord must be in wordSet.
             }
-
+            
             srcSet = tempSet;
             if (srcSet.size() > dstSet.size()) {
                 Set<String> tmp = srcSet;
@@ -151,6 +85,5 @@ class Solution {
         }
         return 0;
     }
-
+    
 }
-
