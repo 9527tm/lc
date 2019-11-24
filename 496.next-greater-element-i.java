8ecaa1956a95f8e6aa4e0e7b61f639b3dff2b865
9527tm <52052033+9527tm@users.noreply.github.com>
2019-11-23 21:18:39 -0800

    update 496.next-greater-element-i.java

---
+++ 496.next-greater-element-i.java
@@ -0,0 +1,85 @@
+/*
+ * @lc app=leetcode id=496 lang=java
+ *
+ * [496] Next Greater Element I
+ *
+ * https://leetcode.com/problems/next-greater-element-i/description/
+ *
+ * algorithms
+ * Easy (60.66%)
+ * Likes:    994
+ * Dislikes: 1609
+ * Total Accepted:    118.3K
+ * Total Submissions: 193.2K
+ * Testcase Example:  '[4,1,2]\n[1,3,4,2]'
+ *
+ * 
+ * You are given two arrays (without duplicates) nums1 and nums2 where nums1’s
+ * elements are subset of nums2. Find all the next greater numbers for nums1's
+ * elements in the corresponding places of nums2. 
+ * 
+ * 
+ * 
+ * The Next Greater Number of a number x in nums1 is the first greater number
+ * to its right in nums2. If it does not exist, output -1 for this number.
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+ * Output: [-1,3,-1]
+ * Explanation:
+ * ⁠   For number 4 in the first array, you cannot find the next greater number
+ * for it in the second array, so output -1.
+ * ⁠   For number 1 in the first array, the next greater number for it in the
+ * second array is 3.
+ * ⁠   For number 2 in the first array, there is no next greater number for it
+ * in the second array, so output -1.
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * Input: nums1 = [2,4], nums2 = [1,2,3,4].
+ * Output: [3,-1]
+ * Explanation:
+ * ⁠   For number 2 in the first array, the next greater number for it in the
+ * second array is 3.
+ * ⁠   For number 4 in the first array, there is no next greater number for it
+ * in the second array, so output -1.
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * All elements in nums1 and nums2 are unique.
+ * The length of both nums1 and nums2 would not exceed 1000.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        return sol1(nums1, nums2); 
+    }
+
+    //O(n * n) / O(1)
+    private int[] sol1(int[] nums1, int[] nums2) {
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            int j = 0;
+            while (j < nums2.length && nums2[j] != nums1[i]) {
+                j++;
+            }
+            int k = j + 1;
+            while (k < nums2.length && nums2[k] < nums2[j]) {
+                k++;
+            }
+            res[i] = k < nums2.length ? nums2[k] : -1;
+        }
+        return res;
+    }
+}
+// @lc code=end
