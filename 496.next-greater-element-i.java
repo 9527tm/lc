6b1bab0b51777340715d3fe942b6128473ca4c92
9527tm <52052033+9527tm@users.noreply.github.com>
2019-11-24 17:55:54 -0800

    update 496.next-greater-element-i.java

--- 496.next-greater-element-i.java
+++ 496.next-greater-element-i.java
@@ -63,7 +63,8 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         //return sol1(nums1, nums2); 
-        return sol2(nums1, nums2); 
+        //return sol2(nums1, nums2); 
+        return sol3(nums1, nums2); 
     }
 
     //O(m * n) / O(1)
@@ -84,7 +85,7 @@ class Solution {
     }
 
     //O(m + n) / O(n)
-    private int[] sol2(int[] nums1, int[] nums2) {
+    private int[] sol2(int[] nums1, int[] nums2) {//backward scanning
         Deque<Integer> stack = new LinkedList<>();
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = nums2.length - 1; i >= 0; i--) {
@@ -99,6 +100,23 @@ class Solution {
         int[] res = new int[nums1.length];
         for (int i = 0; i < nums1.length; i++) {
             res[i] = map.get(nums1[i]);
+        }
+        return res;
+    }
+
+    //O(n + m) / O(n)
+    private int[] sol3(int[] nums1, int[] nums2) {//forward scanning
+        Deque<Integer> stack = new LinkedList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            while (!stack.isEmpty() && stack.peekFirst() < nums2[i]) {
+                map.put(stack.pollFirst(), nums2[i]);
+            }
+            stack.offerFirst(nums2[i]);
+        }
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            res[i] = map.getOrDefault(nums1[i], -1);
         }
         return res;
     }
