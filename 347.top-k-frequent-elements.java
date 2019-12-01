9aa4bf0091e8cefd2b96921a2623890dfa516a33
9527tm <52052033+9527tm@users.noreply.github.com>
2019-11-30 21:35:32 -0800

    update 347.top-k-frequent-elements.java

--- 347.top-k-frequent-elements.java
+++ 347.top-k-frequent-elements.java
@@ -43,18 +43,20 @@
 // @lc code=start
 class Solution {
     public List<Integer> topKFrequent(int[] nums, int k) {
-        return sol1(nums, k); 
+        //return sol1(nums, k); 
+        return sol2(nums, k); 
     }
 
+    //O(n + mlgk) / O(n)
     private List<Integer> sol1(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        for (int n : nums) {
+        for (int n : nums) {//O(n) / O(n)
             map.put(n, map.getOrDefault(n, 0) + 1);
         }
      
         PriorityQueue<Map.Entry<Integer, Integer>> minHeap = 
             new PriorityQueue<>((e1, e2) -> Integer.compare(e1.getValue(), e2.getValue()));
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {//O(mlgk) / O(k)
             if (minHeap.size() < k) {
                 minHeap.offer(entry);
             }
@@ -64,13 +66,48 @@ class Solution {
             }
         }
      
-        List<Integer> res = new ArrayList<>();
-        for (int i = 0; i < k; i++) {
+        List<Integer> res = new ArrayList<>(); 
+        for (int i = 0; i < k; i++) {//O(klgk) / O(k)
             res.add(minHeap.poll().getKey());
         }
-        Collections.reverse(res);
+        Collections.reverse(res); //O(n) / O(1)
         return res;
     }
 
+    //O(n + nlgm) / O(n)
+    private List<Integer> sol2(int[] nums, int k) {
+        TopFreqTracker tracker = new TopFreqTracker();
+        for (int n : nums) {//O(n + nlgm) / O(n)
+            tracker.add(n);
+        }
+        return tracker.get(k);//O(klgm) / O(1)
+    }
+
+    class TopFreqTracker {
+        private Map<Integer, Integer> freqMap = new HashMap<>(); //db
+        private TreeMap<Integer, Set<Integer>> rankMap = new TreeMap<>(Collections.reverseOrder()); //index
+
+        public void add(int n) {//O(lgm) / O(1)
+            int freq = freqMap.getOrDefault(n, 0);//n times: O(n) / O(n)
+            freqMap.put(n, freq + 1);
+            if (freq > 0) {
+                rankMap.get(freq).remove(n);
+            }
+            rankMap.computeIfAbsent(freq + 1, k -> new HashSet<>()).add(n);//n times: O(nlgm) / O(m + n)
+        }
+
+        public List<Integer> get(int k) {//O(klgm) / O(1)
+            List<Integer> res = new ArrayList<>();
+            for (Set<Integer> set : rankMap.values()) {//O(klgm) / O(1)
+                for (int n : set) {//O(k) / O(1)
+                    res.add(n);
+                    if (res.size() >= k) {
+                        return res;
+                    }
+                }
+            }
+            return res;//Never be there
+        }
+    }
 }
 // @lc code=end
